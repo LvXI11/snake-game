c2326ebf8d6fbffe2e9186b9ffc00996850cea34
Lv <releaseinstinct7@qq.com>
2026-08-17 17:50:09 +0800

    完成了大部分

--- snake-game/main.c
+++ snake-game/main.c
@@ -1,6 +1,9 @@
 ﻿#include<stdio.h>
 #include<stdbool.h>
 #include<Windows.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
 
 #define WIDTH 22
 #define HEIGHT 22
@@ -21,7 +24,13 @@ typedef struct {
 	int len;
 	Dir dir;
 }Snake;
+Point food;
+int score=0;
+
 void snake_move(Snake* s);
+Dir get_dir(Dir current);
+void spawn_food(Snake* s);
+
 int main(void) {
 	printf("贪吃蛇准备中....\n"); 
 	Snake s;
@@ -30,12 +39,17 @@ int main(void) {
 	s.body[1].x = 9; s.body[1].y = 10;
 	s.body[2].x = 8; s.body[2].y = 10;
 	s.dir = RIGHT;
+	spawn_food(&s);
 	while (1) {
+		s.dir = get_dir(s.dir);
 		snake_move(&s);
 		system("cls");
 		for (int y = 0; y < HEIGHT; y++) {
 			for (int x = 0; x < WIDTH; x++) {
 				char ch = ' ';
+
+				if (x == food.x && y == food.y)ch = '0';
+
 				bool on_snake = false;
 				for (int i = 0; i < s.len; i++) {
 					if (s.body[i].x == x && s.body[i].y == y) {
@@ -49,7 +63,8 @@ int main(void) {
 			}
 			printf("\n");
 		}
-		Sleep(200);
+		printf("得分：%d", score);
+		Sleep(400);
 	}
 	
 	return 0;
@@ -63,8 +78,59 @@ void snake_move(Snake* s) {
 	case LEFT:new_head.x--; break;
 	case RIGHT:new_head.x++; break;
 	}
+	bool ate=false;
+	if (new_head.x == food.x && new_head.y == food.y)ate = true;
+	if (ate) {
+		s->len++;
+		score++;
+		spawn_food(s);
+	}
 	for (int i = s->len - 1; i > 0; i--) {
 		s->body[i] = s->body[i - 1];
 	}
 	s->body[0] = new_head;
+}
+
+Dir get_dir(Dir current) {
+	Dir new=current;
+	if (_kbhit()) {
+		int key = _getch();
+
+		if (key == 224 || key == 0) {
+			key = _getch();
+			switch (key) {
+			case 72:new=UP; break;
+			case 80:new = DOWN; break;
+			case 75:new = LEFT; break;
+			case 77:new = RIGHT; break;
+			}
+		}
+		else {
+			switch (key) {
+			case 'w':case 'W':new = UP; break;
+			case 's':case 'S':new = DOWN; break;
+			case 'a':case 'A':new = LEFT; break;
+			case 'd':case 'D':new = RIGHT; break;
+			}
+		}
+		if ((current == LEFT && new == RIGHT) ||
+			(current == RIGHT && new == LEFT) ||
+			(current == UP && new == DOWN) ||
+			(current == DOWN && new == UP))
+			return current;
+		return new;
+	}
+	return  current;
+}
+void spawn_food(Snake *s) {
+	srand((unsigned)time(NULL));
+	Point p;
+	p.x = rand() % 20 + 1;
+	p.y = rand() % 20 + 1;
+	int i = s->len;
+	do {
+		if (s->body[i].x == p.x && s->body[i].y == p.y) {
+
+		}
+	} while ();
 }
